84c7a8d47f9e8ab6dc3b3d26a709eeac3e21d45b
Ryder Belserion <no-reply@ryderbelserion.com>
2023-04-06 17:02:59 -0400

    Update build.gradle.kts
    Signed-off-by: Ryder Belserion <no-reply@ryderbelserion.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,4 @@ val beta = settings.versions.beta.get().toBoolean()
 
 val hash = shellRun("git", listOf("rev-parse", "--short", "HEAD"))
 
-rootProject.version = if (beta) hash else "0.0.0.1"
+rootProject.version = if (beta) hash else "0.0.0.2"
