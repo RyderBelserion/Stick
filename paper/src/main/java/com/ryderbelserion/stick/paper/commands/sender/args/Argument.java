32b9b84eec1c59e8438beb2c933e57f8940db670
Ryder Belserion <no-reply@ryderbelserion.com>
2023-07-10 16:13:39 -0400

    Remove command framework from main

--- paper/src/main/java/com/ryderbelserion/stick/paper/commands/sender/args/Argument.java
+++
@@ -1,3 +0,0 @@
-package com.ryderbelserion.stick.paper.commands.sender.args;
-
-public record Argument(String name, int order, ArgumentType argumentType) {}
--- paper/src/main/java/com/ryderbelserion/stick/paper/commands/sender/args/ArgumentType.java
+++
@@ -1,9 +0,0 @@
-package com.ryderbelserion.stick.paper.commands.sender.args;
-
-import java.util.List;
-
-public abstract class ArgumentType {
-
-    public abstract List<String> getPossibleValues();
-
-}
